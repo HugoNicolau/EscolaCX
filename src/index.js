65cecb418b58d320d7900e8deb08e7f12a87e9f2
HugoNicolau <nicolau.hugogiles@gmail.com>
2022-12-12 16:14:57 -0300

    Initial commit

---
+++ src/index.js
@@ -0,0 +1,14 @@
+import express from "express";
+import dotenv from "dotenv";
+import cors from "cors";
+dotenv.config();
+
+
+const app = express();
+app.use(express.json());
+app.use(cors());
+
+
+
+const port = process.env.PORT || 5000;
+app.listen(port, () => console.log(`Server running at port ${port}`));
